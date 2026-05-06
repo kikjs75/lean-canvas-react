f2c06f0caa650eb1fcd78a2286cb6da9efda6fe9
kikjs75 <kikjs75@gmail.com>
2026-05-06 13:45:06 +0900

    React Router

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,50 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './index.css';
-// import App from './App.jsx';
-// import App from './AppCssModules.jsx';
-// import App from './AppStyled.jsx';
-import App from './AppTailwindCSS.jsx';
+import App from './App.jsx';
+import Home from './components/StyledComponents/pages/Home';
+import About from './components/StyledComponents/pages/About';
+import Contact from './components/StyledComponents/pages/Contact';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        path: '',
+        element: <Home />,
+      },
+      {
+        path: 'about',
+        element: <About />,
+      },
+      {
+        path: 'contact',
+        element: <Contact />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
 );
+
+// 1) 기존 방식
+// import { StrictMode } from 'react';
+// import { createRoot } from 'react-dom/client';
+// import './index.css';
+// // import App from './App.jsx';
+// // import App from './AppCssModules.jsx';
+// // import App from './AppStyled.jsx';
+// import App from './AppTailwindCSS.jsx';
+
+// createRoot(document.getElementById('root')).render(
+//   <StrictMode>
+//     <App />
+//   </StrictMode>,
+// );
