261e66f33127d149535932358a4979f0b765c3dc
kikjs75 <kikjs75@gmail.com>
2026-05-06 09:05:55 +0900

    TailwindCSS

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,8 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 // import App from './App.jsx';
 // import App from './AppCssModules.jsx';
-import App from './AppStyled.jsx';
+// import App from './AppStyled.jsx';
+import App from './AppTailwindCSS.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
