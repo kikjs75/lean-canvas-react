39f946f36b699f2af56545c4229e17fa9711b11c
kikjs75 <kikjs75@gmail.com>
2026-05-10 10:03:21 +0900

    등록구현, 공통 컴포넌트, dayjs 라이브러리-1-createCanvas, 등록하기 버튼

--- src/api/canvas.js
+++ src/api/canvas.js
@@ -1,6 +1,24 @@
+import dayjs from 'dayjs';
 import { canvases } from './http';
+import { v4 as uuid4 } from 'uuid';
 
 // 목록
 export function getCanvases(params) {
-  return canvases.get('/', { params: params });
+  const payload = Object.assign(
+    {
+      _sort: 'lastModified',
+      _order: 'desc',
+    },
+    params,
+  );
+  return canvases.get('/', { params: payload });
+}
+
+export function createCanvas() {
+  const newCanvas = {
+    title: uuid4().substring(0, 4) + '_새로운 린 캔버스',
+    lastModified: dayjs().format('YYYY-MM-DD HH:mm:ss'),
+    category: '신규',
+  };
+  return canvases.post('/', newCanvas);
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react';
 import CanvasList from '../components/CanvasList';
 import SearchBar from '../components/SearchBar';
 import ViewToggle from '../components/ViewToggle';
-import { getCanvases } from '../api/canvas';
+import { getCanvases, createCanvas } from '../api/canvas';
 import Loading from '../components/Loading';
 import Error from '../components/Error';
 
@@ -14,27 +14,56 @@ function Home() {
   const [error, setError] = useState(null);
   const [retryCount, setRetryCount] = useState(0);
 
-  // 5) (axios)경고 없음. 실행 정상. : response 의 data 응답 데이터 있고 그 외 config, header, request, status, statusText 등 추가 정보 더 있음. fetch 에는 없음.
-  useEffect(() => {
-    async function fetchData(params) {
+  async function fetchData(params) {
+    try {
       setIsLoading(true);
       setError(null);
-      try {
-        await new Promise(resolver => setTimeout(resolver, 2000));
 
-        const response = await getCanvases(params);
-        console.log('response: ', response);
-        setData(response.data);
-      } catch (err) {
-        setError(err);
-      } finally {
-        setIsLoading(false);
-      }
+      await new Promise(resolver => setTimeout(resolver, 1000));
+
+      const response = await getCanvases(params);
+      console.log('response: ', response);
+      setData(response.data);
+    } catch (err) {
+      setError(err);
+    } finally {
+      setIsLoading(false);
     }
-    fetchData({ title_like: searchText });
+  }
+
+  // async function fetchDataOther(params) {
+  //   setIsLoading(true);
+  //   setError(null);
+
+  //   await new Promise(resolver => setTimeout(resolver, 2000));
+
+  //   return getCanvases(params)
+  //     .then(response => {
+  //       console.log('response: ', response);
+  //       setData(response.data);
+  //     })
+  //     .catch(err => {
+  //       setError(err);
+  //     })
+  //     .finally(() => {
+  //       setIsLoading(false);
+  //     });
+  // }
+
+  // 5) (axios)경고 없음. 실행 정상. : response 의 data 응답 데이터 있고 그 외 config, header, request, status, statusText 등 추가 정보 더 있음. fetch 에는 없음.
+  useEffect(() => {
+    const loadData = async () => {
+      await fetchData({ title_like: searchText });
+    };
+    loadData();
   }, [searchText, retryCount]);
 
   const handleDelete = id => setData(data.filter(item => item.id !== id));
+
+  const handleAddCanvas = () => {
+    createCanvas();
+    fetchData({ title_like: searchText });
+  };
 
   return (
     <>
@@ -42,6 +71,8 @@ function Home() {
         <SearchBar searchText={searchText} setSearchText={setSearchText} />
         <ViewToggle isGridView={isGridView} setIsGridView={setIsGridView} />
       </div>
+
+      <button onClick={handleAddCanvas}>등록하기</button>
 
       {isLoading && <Loading />}
       {error && (
